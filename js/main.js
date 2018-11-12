dc402382b6f1dd4c90a947f3112d7ea265909967
Christopher Salinas <salinas23cfh@gmail.com>
2018-11-11 20:12:23 -0600

    working on services worker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ let restaurants,
 var newMap
 var markers = []
 
+/**registering service worker*/
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(function(err) {
+    console.error(err);
+  });
+}
+
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
